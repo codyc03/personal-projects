e6e814580236bc906759fbfa53622a659afccb35
Cody Christensen <cody.christensensc17@gmail.com>
2024-05-23 22:17:26 -0600

    Starting to learn how to integrate SQL commands and connect to PostgreSQL database

--- MentalHealthTracker/Main/Main.py
+++ MentalHealthTracker/Main/Main.py
@@ -1,1 +1,28 @@
+import psycopg2
 
+conn = psycopg2.connect(
+                                  dbname = "mydatabase",
+                                  user = "postgres",
+                                  password = "12345678",
+                                  host = "localhost"
+                                  )
+
+cur = conn.cursor()
+
+cur.execute(
+    """
+    CREATE TABLE mytable2 (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+    
+    )
+
+    """
+    ) 
+
+conn.commit()
+
+cur.close()
+conn.close()
+
+print()
