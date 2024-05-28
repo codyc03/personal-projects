0ebf8f5901f999c80b90a50fb0487a5d1912eaf8
Cody Christensen <cody.christensensc17@gmail.com>
2024-05-28 13:35:37 -0600

    Progress on grid display

--- MentalHealthTracker/MainPageGUI/MainPageGUI.py
+++ MentalHealthTracker/MainPageGUI/MainPageGUI.py
@@ -11,10 +11,6 @@ def exit_program(event=None):
 root = tk.Tk()
 root.title("Main Window")
 
-# Get the screen dimensions
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
 # Set the window attributes to fullscreen
 root.attributes("-fullscreen", True)
 
@@ -31,13 +27,13 @@ root.bind("<Configure>", on_resize)
 root.bind("<Escape>", exit_program)
 
 greeting = ttk.Label(master=frame, text="Hello, welcome to MyHealth!", font=("Arial", 20, "bold"))
-greeting.pack()
+greeting.grid(row=0,column=0,columnspan=3, sticky='nsew')
 
 button = ttk.Button(master=frame, text="Add Entry")
-button.pack()
+button.grid(row=1,column=1, sticky='nsew')
 
 goals_label = ttk.Labelframe(master=frame, text="Goals")
-goals_label.pack()
+goals_label.grid(row=1,column=0, sticky='nsew')
 
 goals_output_1 = ttk.Label(master=goals_label, text="First goal")
 goals_output_1.pack()
@@ -49,7 +45,7 @@ goals_output_3 = ttk.Label(master=goals_label, text="Third goal")
 goals_output_3.pack()
 
 accomplishments_label = ttk.Labelframe(master=frame, text="accomplishments")
-accomplishments_label.pack()
+accomplishments_label.grid(row=2,column=0, sticky='nsew')
 
 accomplishments_output_1 = ttk.Label(master=accomplishments_label, text="First accomplishment")
 accomplishments_output_1.pack()
@@ -61,7 +57,7 @@ accomplishments_output_3 = ttk.Label(master=accomplishments_label, text="Third a
 accomplishments_output_3.pack()
 
 coping_strategies_label = ttk.Labelframe(master=frame, text="coping_strategies")
-coping_strategies_label.pack()
+coping_strategies_label.grid(row=3,column=0, sticky='nsew')
 
 coping_strategies_output_1 = ttk.Label(master=coping_strategies_label, text="First coping strategy")
 coping_strategies_output_1.pack()
@@ -72,9 +68,8 @@ coping_strategies_output_2.pack()
 coping_strategies_output_3 = ttk.Label(master=coping_strategies_label, text="Third coping strategy")
 coping_strategies_output_3.pack()
 
-
 gratitudes_label = ttk.Labelframe(master=frame, text="gratitudes")
-gratitudes_label.pack()
+gratitudes_label.grid(row=1,column=2, sticky='nsew')
 
 gratitudes_output_1 = ttk.Label(master=gratitudes_label, text="First gratitude")
 gratitudes_output_1.pack()
@@ -86,7 +81,7 @@ gratitudes_output_3 = ttk.Label(master=gratitudes_label, text="Third gratitude")
 gratitudes_output_3.pack()
 
 reflections_label = ttk.Labelframe(master=frame, text="reflections")
-reflections_label.pack()
+reflections_label.grid(row=2,column=2, sticky='nsew')
 
 reflections_output_1 = ttk.Label(master=reflections_label, text="First reflection")
 reflections_output_1.pack()
@@ -98,7 +93,7 @@ reflections_output_3 = ttk.Label(master=reflections_label, text="Third reflectio
 reflections_output_3.pack()
 
 self_care_activities_label = ttk.Labelframe(master=frame, text="self_care_activities")
-self_care_activities_label.pack()
+self_care_activities_label.grid(row=3,column=2, sticky='nsew')
 
 self_care_activities_output_1 = ttk.Label(master=self_care_activities_label, text="First self_care_activity")
 self_care_activities_output_1.pack()
@@ -109,16 +104,16 @@ self_care_activities_output_2.pack()
 self_care_activities_output_3 = ttk.Label(master=self_care_activities_label, text="Third self_care_activity")
 self_care_activities_output_3.pack()
 
-stressors_label = ttk.Labelframe(master=frame, text="stressors")
-stressors_label.pack()
+# stressors_label = ttk.Labelframe(master=frame, text="stressors")
+# stressors_label.grid(row=4,column=2)
 
-stressors_output_1 = ttk.Label(master=stressors_label, text="First stressor")
-stressors_output_1.pack()
+# stressors_output_1 = ttk.Label(master=stressors_label, text="First stressor")
+# stressors_output_1.pack()
 
-stressors_output_2 = ttk.Label(master=stressors_label, text="Second stressor")
-stressors_output_2.pack()
+# stressors_output_2 = ttk.Label(master=stressors_label, text="Second stressor")
+# stressors_output_2.pack()
 
-stressors_output_3 = ttk.Label(master=stressors_label, text="Third stressor")
-stressors_output_3.pack()
+# stressors_output_3 = ttk.Label(master=stressors_label, text="Third stressor")
+# stressors_output_3.pack()
 
 root.mainloop()
