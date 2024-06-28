80408f5be9f3cf52d8f2fdda73649743659532f7
Cody Christensen <cody.christensensc17@gmail.com>
2024-06-28 00:17:34 -0600

    Changed all the sql statements to one private helper method to simplify error handling in Main

--- MentalHealthTracker/Main/Main.py
+++ MentalHealthTracker/Main/Main.py
@@ -18,6 +18,7 @@ db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("HOST_NAME")
 
 
+
 try :
     conn = psycopg2.connect(
                                     dbname = db_name,
@@ -35,7 +36,7 @@ if(conn == NULL) :
 cur = conn.cursor()
 
 def get_latest_entry_date() :
-    cur.execute("SELECT entry_date FROM user_entries WHERE username = 'codyc' ORDER BY entry_date DESC LIMIT 1")
+    execute_statement("SELECT entry_date FROM user_entries WHERE username = 'codyc' ORDER BY entry_date DESC LIMIT 1")
     rows = cur.fetchall()
 
     result_strings = []
@@ -48,7 +49,7 @@ def get_latest_entry_date() :
 
 
 def get_reminders(attribute,table) : 
-    cur.execute(f"SELECT {attribute} FROM {table} WHERE username = 'codyc'")
+    execute_statement(f"SELECT {attribute} FROM {table} WHERE username = 'codyc'")
     rows = cur.fetchall()
 
     result_strings = []
@@ -59,7 +60,7 @@ def get_reminders(attribute,table) :
     return result_strings
 
 def get_entries(attribute,table) : 
-    cur.execute(f"SELECT {attribute} FROM {table} WHERE username = 'codyc' ORDER BY entry_date DESC" )
+    execute_statement(f"SELECT {attribute} FROM {table} WHERE username = 'codyc' ORDER BY entry_date DESC" )
     rows = cur.fetchall()
 
     result_strings = []
@@ -69,56 +70,56 @@ def get_entries(attribute,table) :
     return result_strings
 
 def add_user() :
-    cur.execute(f"INSERT INTO users_table (name) VALUES ('Test User')")
+    execute_statement(f"INSERT INTO users_table (name) VALUES ('Test User')")
 
 def remove_user() :
-    cur.execute(f"DELETE FROM users_table WHERE name = 'Test User'")
+    execute_statement(f"DELETE FROM users_table WHERE name = 'Test User'")
 
 def add_entry(mood,energy_level,sleep_duration,sleep_quality, physical_symptoms, social_interaction,physical_activity) :
-    cur.execute(f"INSERT INTO user_entries (mood, energy_level, sleep_duration, sleep_quality, physical_symptoms, social_interaction, physical_activity, username) VALUES ({mood}, {energy_level}, '{sleep_duration} hours', {sleep_quality}, {physical_symptoms}, '{social_interaction} hours', '{physical_activity} hours', 'codyc')")
+    execute_statement(f"INSERT INTO user_entries (mood, energy_level, sleep_duration, sleep_quality, physical_symptoms, social_interaction, physical_activity, username) VALUES ({mood}, {energy_level}, '{sleep_duration} hours', {sleep_quality}, {physical_symptoms}, '{social_interaction} hours', '{physical_activity} hours', 'codyc')")
     conn.commit()
     
 def add_accomplishment(accomplishment) :
-    cur.execute(f"INSERT INTO accomplishments_new (accomplishment,username) VALUES ('{accomplishment}', 'codyc')")
+    execute_statement(f"INSERT INTO accomplishments_new (accomplishment,username) VALUES ('{accomplishment}', 'codyc')")
 
 def remove_accomplishment(accomplishment) :
-    cur.execute(f"DELETE FROM accomplishments_new WHERE accomplishment = '{accomplishment}' AND username = 'codyc'")
+    execute_statement(f"DELETE FROM accomplishments_new WHERE accomplishment = '{accomplishment}' AND username = 'codyc'")
     
 def add_coping_strategy(coping_strategy) :
-    cur.execute(f"INSERT INTO coping_strategies_new (coping_strategy, username) VALUES ('{coping_strategy}', 'codyc')")
+    execute_statement(f"INSERT INTO coping_strategies_new (coping_strategy, username) VALUES ('{coping_strategy}', 'codyc')")
 
 def remove_coping_strategy(coping_strategy) :
-    cur.execute(f"DELETE FROM coping_strategies_new WHERE coping_strategy = '{coping_strategy}' AND username = 'codyc'")
+    execute_statement(f"DELETE FROM coping_strategies_new WHERE coping_strategy = '{coping_strategy}' AND username = 'codyc'")
     
 def add_goal(goal) :
-    cur.execute(f"INSERT INTO goals_new (goal, username) VALUES ('{goal}','codyc')")
+    execute_statement(f"INSERT INTO goals_new (goal, username) VALUES ('{goal}','codyc')")
 
 def remove_goal(goal) :
-    cur.execute(f"DELETE FROM goals_new WHERE goal = '{goal}' AND username = 'codyc'")
+    execute_statement(f"DELETE FROM goals_new WHERE goal = '{goal}' AND username = 'codyc'")
 
 def add_gratitude(gratitude) :
-    cur.execute(f"INSERT INTO gratitudes_new (gratitude,username) VALUES ('{gratitude}', 'codyc')")
+    execute_statement(f"INSERT INTO gratitudes_new (gratitude,username) VALUES ('{gratitude}', 'codyc')")
 
 def remove_gratitude(gratitude) :
-    cur.execute(f"DELETE FROM gratitudes_new WHERE gratitude = '{gratitude}' AND username = 'codyc'")
+    execute_statement(f"DELETE FROM gratitudes_new WHERE gratitude = '{gratitude}' AND username = 'codyc'")
 
 def add_reflection(reflection) :
-    cur.execute(f"INSERT INTO reflections_new (reflection,username) VALUES ('{reflection}', 'codyc')")
+    execute_statement(f"INSERT INTO reflections_new (reflection,username) VALUES ('{reflection}', 'codyc')")
 
 def remove_reflection(reflection) :
-    cur.execute(f"DELETE FROM reflections_new WHERE reflection = '{reflection}' AND username = 'codyc'")
+    execute_statement(f"DELETE FROM reflections_new WHERE reflection = '{reflection}' AND username = 'codyc'")
 
 def add_self_care_activity(self_care_activity) :
-    cur.execute(f"INSERT INTO self_care_activities_new (self_care_activity, username) VALUES ('{self_care_activity}','codyc')")
+    execute_statement(f"INSERT INTO self_care_activities_new (self_care_activity, username) VALUES ('{self_care_activity}','codyc')")
 
 def remove_self_care_activity(self_care_activity) :
-    cur.execute(f"DELETE FROM self_care_activities_new WHERE self_care_activity = '{self_care_activity}' AND username = 'codyc'")
+    execute_statement(f"DELETE FROM self_care_activities_new WHERE self_care_activity = '{self_care_activity}' AND username = 'codyc'")
 
 # def add_stressor() :
-#     cur.execute(f"INSERT INTO stressors (id, stressor) VALUES (1, 'Love')")
+#     execute_statement(f"INSERT INTO stressors (id, stressor) VALUES (1, 'Love')")
 
 # def remove_stressor() :
-#     cur.execute(f"DELETE FROM stressors WHERE stressor = 'Love'")
+#     execute_statement(f"DELETE FROM stressors WHERE stressor = 'Love'")
 
 def get_avg(category) :
     days = [10,30,90]
@@ -143,37 +144,37 @@ def get_avg(category) :
         total = 0
         
         if category == 0:  # Assuming category 0 represents mood
-            cur.execute(f"SELECT mood FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
+            execute_statement(f"SELECT mood FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
             rows = cur.fetchall()
             total = fetch_helper(rows)
          
         if category == 1 :
-            cur.execute(f"SELECT energy_level FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
+            execute_statement(f"SELECT energy_level FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
             rows = cur.fetchall()
             total = fetch_helper(rows)
 
         if category == 2 :
-            cur.execute(f"SELECT sleep_duration FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
+            execute_statement(f"SELECT sleep_duration FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
             rows = cur.fetchall()
             total = fetch_helper(rows)
             
         if category == 3 :
-            cur.execute(f"SELECT sleep_quality FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
+            execute_statement(f"SELECT sleep_quality FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
             rows = cur.fetchall()
             total = fetch_helper(rows)
             
         if category == 4 :
-            cur.execute(f"SELECT physical_symptoms FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
+            execute_statement(f"SELECT physical_symptoms FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
             rows = cur.fetchall()
             total = fetch_helper(rows)
             
         if category == 5 :
-            cur.execute(f"SELECT social_interaction FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
+            execute_statement(f"SELECT social_interaction FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
             rows = cur.fetchall()
             total = fetch_helper(rows)
 
         if category == 6 :
-            cur.execute(f"SELECT physical_activity FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
+            execute_statement(f"SELECT physical_activity FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
             rows = cur.fetchall()
             total = fetch_helper(rows)
         
@@ -211,19 +212,19 @@ def get_cmp(category, ten_day_average, thirty_day_average, ninety_day_average) :
         value = 0
         
         if category == 0:  # Assuming category 0 represents mood
-            cur.execute("SELECT mood FROM user_entries ORDER BY entry_date DESC LIMIT 1")
+            execute_statement("SELECT mood FROM user_entries ORDER BY entry_date DESC LIMIT 1")
         elif category == 1:
-            cur.execute("SELECT energy_level FROM user_entries ORDER BY entry_date DESC LIMIT 1")
+            execute_statement("SELECT energy_level FROM user_entries ORDER BY entry_date DESC LIMIT 1")
         elif category == 2:
-            cur.execute("SELECT sleep_duration FROM user_entries ORDER BY entry_date DESC LIMIT 1")
+            execute_statement("SELECT sleep_duration FROM user_entries ORDER BY entry_date DESC LIMIT 1")
         elif category == 3:
-            cur.execute("SELECT sleep_quality FROM user_entries ORDER BY entry_date DESC LIMIT 1")
+            execute_statement("SELECT sleep_quality FROM user_entries ORDER BY entry_date DESC LIMIT 1")
         elif category == 4:
-            cur.execute("SELECT physical_symptoms FROM user_entries ORDER BY entry_date DESC LIMIT 1")
+            execute_statement("SELECT physical_symptoms FROM user_entries ORDER BY entry_date DESC LIMIT 1")
         elif category == 5:
-            cur.execute("SELECT social_interaction FROM user_entries ORDER BY entry_date DESC LIMIT 1")
+            execute_statement("SELECT social_interaction FROM user_entries ORDER BY entry_date DESC LIMIT 1")
         elif category == 6:
-            cur.execute("SELECT physical_activity FROM user_entries ORDER BY entry_date DESC LIMIT 1")
+            execute_statement("SELECT physical_activity FROM user_entries ORDER BY entry_date DESC LIMIT 1")
 
         row = cur.fetchone()
         
@@ -247,7 +248,7 @@ def feed_values():
         entry_date = datetime.date.today() - datetime.timedelta(days=i + 1)
         
         # Execute the SQL query with dynamically generated date
-        cur.execute("""
+        execute_statement("""
             INSERT INTO user_entries (entry_date, mood, energy_level, sleep_duration, sleep_quality, physical_symptoms, social_interaction, physical_activity, username)
             VALUES (%s, 10, 10, '10 hours', 10, 10, '10 hours', '10 hours', 'codyc')
         """, (entry_date,))
@@ -255,6 +256,12 @@ def feed_values():
 def test() : 
      print( "hello")
     
+
+def execute_statement(statement) :
+    cur.execute(statement)
+
+
+
 
 # conn.commit()
 
