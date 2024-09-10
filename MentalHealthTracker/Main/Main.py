0430f3245f7f89c86b75ceab6226396fb7176830
Cody Christensen <cody.christensensc17@gmail.com>
2024-09-10 04:55:30 -0600

    Cleaned comments in Main

--- MentalHealthTracker/Main/Main.py
+++ MentalHealthTracker/Main/Main.py
@@ -1,7 +1,7 @@
 """ 
 File:      Main.py 
 Author:    Cody Christensen 
-Date:      8/29/24 
+Date:      9/10/24 
 Copyright: Cody Christensen - This work may not be copied for use in Academic Coursework. 
   
 I, Cody Christensen, certify that I wrote this code  
@@ -33,6 +33,9 @@ def failed_connection() :
      sys.exit()
 
 def get_latest_entry_date() :
+    """
+    Retrieves the date representing the most recent entry in user entries.
+    """
     execute_statement("SELECT entry_date FROM user_entries WHERE username = 'codyc' ORDER BY entry_date DESC LIMIT 1")
     rows = cur.fetchall()
 
@@ -44,6 +47,9 @@ def get_latest_entry_date() :
     return result_strings
 
 def get_reminders(attribute,table) : 
+    """
+    Helper method used to fetch different reminders from the database.
+    """
     execute_statement(f"SELECT {attribute} FROM {table} WHERE username = 'codyc'")
     rows = cur.fetchall()
 
@@ -55,62 +61,113 @@ def get_reminders(attribute,table) :
     return result_strings
 
 def get_entries(attribute,table) : 
+    """
+    Helper method to get specificed entries from specified table.
+    """
     execute_statement(f"SELECT {attribute} FROM {table} WHERE username = 'codyc' ORDER BY entry_date DESC" )
     rows = cur.fetchall()
 
     result_strings = []
+    
     for row in rows:
         result_strings.append(row[0])
 
     return result_strings
 
 def add_user() :
+    """
+    Test method used to add a user to the user entries table.
+    """
     execute_statement(f"INSERT INTO users_table (name) VALUES ('Test User')")
 
 def remove_user() :
+    """
+    Test method used to remove a user from the user entries table.
+    """
     execute_statement(f"DELETE FROM users_table WHERE name = 'Test User'")
 
 def add_entry(mood,energy_level,sleep_duration,sleep_quality, physical_symptoms, social_interaction,physical_activity) :
+    """
+    Add an entry into the user entries table.
+    """
     execute_statement(f"INSERT INTO user_entries (mood, energy_level, sleep_duration, sleep_quality, physical_symptoms, social_interaction, physical_activity, username) VALUES ({mood}, {energy_level}, '{sleep_duration} hours', {sleep_quality}, {physical_symptoms}, '{social_interaction} hours', '{physical_activity} hours', 'codyc')")
-    conn.commit()
     
 def add_accomplishment(accomplishment) :
+    """
+    Add an accomplishment to the accopmlishments table.
+    """
     execute_statement(f"INSERT INTO accomplishments_new (accomplishment,username) VALUES ('{accomplishment}', 'codyc')")
 
 def remove_accomplishment(accomplishment) :
+    """
+    Remove an accomplishment from the accopmlishments table.
+    """
     execute_statement(f"DELETE FROM accomplishments_new WHERE accomplishment = '{accomplishment}' AND username = 'codyc'")
     
 def add_coping_strategy(coping_strategy) :
+    """
+    Add a coping strategy to the coping strategies table.
+    """
     execute_statement(f"INSERT INTO coping_strategies_new (coping_strategy, username) VALUES ('{coping_strategy}', 'codyc')")
 
 def remove_coping_strategy(coping_strategy) :
+    """
+    Remove a coping strategy from the coping strategies table.
+    """
     execute_statement(f"DELETE FROM coping_strategies_new WHERE coping_strategy = '{coping_strategy}' AND username = 'codyc'")
     
 def add_goal(goal) :
+    """
+    Add a goal to the goals table.
+    """
     execute_statement(f"INSERT INTO goals_new (goal, username) VALUES ('{goal}','codyc')")
 
 def remove_goal(goal) :
+    """
+    Remove a goal from the goals table.
+    """
     execute_statement(f"DELETE FROM goals_new WHERE goal = '{goal}' AND username = 'codyc'")
 
 def add_gratitude(gratitude) :
+    """
+    Add a gratitude to the gratitudes table.
+    """
     execute_statement(f"INSERT INTO gratitudes_new (gratitude,username) VALUES ('{gratitude}', 'codyc')")
 
 def remove_gratitude(gratitude) :
+    """
+    Remove a gratitude from the gratitudes table.
+    """
     execute_statement(f"DELETE FROM gratitudes_new WHERE gratitude = '{gratitude}' AND username = 'codyc'")
 
 def add_reflection(reflection) :
+    """
+    Add a reflection to the reflections table.
+    """
     execute_statement(f"INSERT INTO reflections_new (reflection,username) VALUES ('{reflection}', 'codyc')")
 
 def remove_reflection(reflection) :
+    """
+    Remove a reflection from the reflections table.
+    """
     execute_statement(f"DELETE FROM reflections_new WHERE reflection = '{reflection}' AND username = 'codyc'")
 
 def add_self_care_activity(self_care_activity) :
+    """
+    Add a self care activity to the self care activities table.
+    """
     execute_statement(f"INSERT INTO self_care_activities_new (self_care_activity, username) VALUES ('{self_care_activity}','codyc')")
 
 def remove_self_care_activity(self_care_activity) :
+    """
+    Remove a self care activity from the self care activities table.
+    """
     execute_statement(f"DELETE FROM self_care_activities_new WHERE self_care_activity = '{self_care_activity}' AND username = 'codyc'")
 
 def get_avg(category) :
+    """
+    Returns an array with the 10 day, 30 day, and 90 day average of the specified category.
+    """
     days = [10,30,90]
     ten_day_average = 0
     thirty_day_average = 0
@@ -132,7 +189,7 @@ def get_avg(category) :
     for i in range(len(days)) :  
         total = 0
         
-        if category == 0:  # Assuming category 0 represents mood
+        if category == 0:  
             execute_statement(f"SELECT mood FROM user_entries ORDER BY entry_date DESC LIMIT {days[i]}")
             rows = cur.fetchall()
             total = fetch_helper(rows)
@@ -176,17 +233,19 @@ def get_avg(category) :
         elif i == 2 : 
             ninety_day_average = avg
 
-    # return f"{ten_day_average}\n{thirty_day_average}\n{ninety_day_average}"
     return [ten_day_average,thirty_day_average,ninety_day_average]        
 
 def get_cmp(category, ten_day_average, thirty_day_average, ninety_day_average) :    
+    """
+    Returns an array with the 10 day, 30 day, and 90 day comparison to average of the specified category.
+    """
+    
     days = [10,30,90]
     
     def comparison(most_recent, days, previous_avg) :
         comp = most_recent / previous_avg 
-        # Assuming you have today's value and average value calculated
-        today_value = most_recent # Replace with your actual value for today's entry
-        average_value = previous_avg  # Replace with your actual average value over the past ninety days
+        today_value = most_recent 
+        average_value = previous_avg  
         
         if isinstance(today_value, datetime.timedelta):
             today_value = today_value.total_seconds() / 3600
@@ -200,7 +259,7 @@ def get_cmp(category, ten_day_average, thirty_day_average, ninety_day_average) :
     for i in range(len(days)) :  
         value = 0
         
-        if category == 0:  # Assuming category 0 represents mood
+        if category == 0: 
             execute_statement("SELECT mood FROM user_entries ORDER BY entry_date DESC LIMIT 1")
         elif category == 1:
             execute_statement("SELECT energy_level FROM user_entries ORDER BY entry_date DESC LIMIT 1")
@@ -218,7 +277,7 @@ def get_cmp(category, ten_day_average, thirty_day_average, ninety_day_average) :
         row = cur.fetchone()
         
         if row:
-            value = row[0]  # Assuming each query returns exactly one value
+            value = row[0]  
         else:
             value = 0
      
@@ -232,6 +291,9 @@ def get_cmp(category, ten_day_average, thirty_day_average, ninety_day_average) :
     return [ten_day_comparison,thirty_day_comparison,ninety_day_comparison]
 
 def feed_values():
+    """
+    Method used during testing to feed values into the user entries table, all with different timestamps.
+    """
     for i in range(100):
         # Generate a date for each iteration, starting from today and decrementing by 'i' days
         entry_date = datetime.date.today() - datetime.timedelta(days=i)
@@ -246,21 +308,18 @@ def feed_values():
     
         # Execute the SQL query with dynamically generated date
         execute_statement(sql)
-
-# Close the database connection
-    conn.close()
         
-def test() : 
-     print( "hello") 
-
 def execute_statement(statement) :
+    """
+    Method used to execute any generic SQL statement using the cursor.
+    """
     try :
         cur.execute(statement)
     except Exception :
         if not error_callback == 0 :
             error_callback()
         
-
+#Start with defining as null
 conn = NULL
 cur = NULL
 error_callback = 0
@@ -274,6 +333,7 @@ db_user = os.getenv("USER_NAME")
 db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("HOST_NAME")
 
+# Try to connect to database
 try :
     conn = psycopg2.connect(
                                     dbname = db_name,
@@ -285,9 +345,10 @@ try :
 except Exception as e:
     failed_connection()
 
+# Extra checking to make sure connection was succesful and assigned
 if(conn == NULL) :
     failed_connection()
     
-
+# Declare cursor
 cur = conn.cursor()
 
